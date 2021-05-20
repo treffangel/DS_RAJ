587e00ed9aee7b1a993f73f20ed1a42440282ce9
treffangel <treffer@th-brandenburg.de>
2021-05-20 09:50:02 +0200

    Exercise 3 Julia

---
+++ ex3TwoExercise.jl
@@ -0,0 +1,15 @@
+#=
+ex3TwoExercise:
+- Julia version: 1.6.1
+- Author: Angie
+- Date: 2021-05-20
+- Task: Create a 2x4 two dimensional matrix with random floats in it and in the next step determine the biggest element.
+=#
+
+mx = rand(Float64, (2, 4))
+
+findmax(mx)
+
+
+
+
